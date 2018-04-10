5ec2f230c80e65c09b9972d0b64ae3c5ce206b17
Spányik Balázs <bspanyik@loginet.hu>
2018-04-10 16:53:59 +0200

    I've got a Robot, I've got a Room - UGH! - Robot Room!

---
+++ src/Entity/Robot.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Area51\Entity;
+
+class Robot
+{
+    const MAX_MOVE_DISTANCE = 5;
+
+    /** @var string */
+    protected $id;
+
+    /** @var string */
+    protected $operator = '';
+
+    /** @var int */
+    protected $salary = 0;
+
+    /** @var bool */
+    protected $escaped = false;
+
+    /** @var Room */
+    protected $room;
+
+    /**
+     * Robot constructor.
+     * @param string $id
+     * @param string $operator
+     * @param Room $room
+     */
+    public function __construct(string $id, string $operator, Room $room)
+    {
+        $this->id = $id;
+        $this->operator = $operator;
+        $this->room = $room;
+    }
+}
---
+++ src/Entity/Room.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Area51\Entity;
+
+class Room
+{
+    /** @var int */
+    protected $width;
+
+    /** @var int */
+    protected $height;
+
+    /** @var int */
+    protected $robotX;
+
+    /** @var int */
+    protected $robotY;
+
+    public function __construct($width, $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+        $this->initRobot();
+    }
+
+    protected function initRobot()
+    {
+        $this->robotX = rand(0, $this->width - 1);
+        $this->robotY = rand(0, $this->height - 1);
+    }
+
+}
