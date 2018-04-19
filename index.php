6f21df4a393950f6bbb724a02793c84418f958e9
Spányik Balázs <bspanyik@loginet.hu>
2018-04-19 03:51:22 +0200

    Put the routing together in the Front Controller, yay!

--- index.php
+++ index.php
@@ -8,3 +8,19 @@ $robot = $robotFactory->make('akárki@valami.hu');
 echo 'Welcome to Area 51...';
 echo '<pre>' . print_r($robot, true) . '</pre>';
 
+$routes = require 'config/routes.php';
+
+try {
+    $container = new Area51\System\Container();
+    $context   = new Area51\System\Routing\Context();
+    $router    = new Area51\System\Routing\Router($container, $context, $routes);
+
+    $controller = $router->getController();
+    $parameters = $router->getParameters();
+
+    call_user_func_array($controller, $parameters);
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
+
+exit;
