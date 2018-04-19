7f27ec29f54e4788a139dd70209210c379cf7870
Spányik Balázs <bspanyik@loginet.hu>
2018-04-19 03:48:38 +0200

    The yet very empty RobotController with placeholder messages

---
+++ src/Controller/RobotController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Area51\Controller;
+
+class RobotController
+{
+    public function createAction()
+    {
+        die('New Robot Created!');
+    }
+
+    public function moveAction()
+    {
+        die('The Robot Moves!');
+    }
+
+    public function escapeAction()
+    {
+        die('The Robot Escapes');
+    }
+}
