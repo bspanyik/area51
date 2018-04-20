f7afbf07a612bf171ef7f646ba89a85ea5c14815
Spányik Balázs <bspanyik@loginet.hu>
2018-04-20 20:42:25 +0200

    Start collecting our dependencies for our Container

---
+++ config/services.php
@@ -0,0 +1,29 @@
+<?php
+use Area51\Factory\RobotFactory;
+use Area51\Factory\RoomFactory;
+use Area51\System\Container;
+use Area51\Validator\EmailValidator;
+use Area51\Manager\RobotStorageManager;
+use Area51\Manager\RobotCreationManager;
+
+return [
+    EmailValidator::class => function() {
+        return new EmailValidator();
+    },
+
+    RoomFactory::class => function() {
+        return new RoomFactory();
+    },
+
+    RobotFactory::class => function(Container $c) {
+        return new RobotFactory($c->get(RoomFactory::class));
+    },
+
+    RobotStorageManager::class => function() {
+        return new RobotStorageManager(__DIR__ . '/../storage');
+    },
+
+    RobotCreationManager::class => function(Container $c) {
+        return new RobotCreationManager($c->get(RobotStorageManager::class), $c->get(RobotFactory::class));
+    },
+];
