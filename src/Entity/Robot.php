bf5efcd285c68256bdee527affb762a29170e9c5
Spányik Balázs <bspanyik@loginet.hu>
2018-05-08 20:31:56 +0200

    This is how our Robot moves in the Room
    Robot got a bit prettier with PHPDoc blocks. It now has a move()
    method with limiting distance and letting the Room to handle the
    movement.
    
    Room now knows about valid directions, and handles the Robot while
    it tries to move. It also got prettier as well.

--- src/Entity/Robot.php
+++ src/Entity/Robot.php
@@ -6,26 +6,37 @@ class Robot
 {
     const MAX_MOVE_DISTANCE = 5;
 
-    /** @var string */
+    /**
+     * @var string
+     */
     private $id;
 
-    /** @var string */
+    /**
+     * @var string
+     */
     private $operator = '';
 
-    /** @var int */
+    /**
+     * @var int
+     */
     private $cycle = 0;
 
-    /** @var int */
+    /**
+     * @var int
+     */
     private $salary = 0;
 
-    /** @var bool */
+    /**
+     * @var bool
+     */
     private $escaped = false;
 
-    /** @var Room */
+    /**
+     * @var Room
+     */
     private $room;
 
     /**
-     * Robot constructor.
      * @param string $id
      * @param string $operator
      * @param Room $room
@@ -38,14 +49,34 @@ class Robot
         $this->room = $room;
     }
 
+    /**
+     * @return string
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * increases roboty lifecycle when robot wakes up
+     */
     public function __wakeup()
     {
         $this->cycle += 1;
+    }
+
+    /**
+     * asks Room to control Robot movement
+     *
+     * @param string $direction
+     * @param int $distance
+     * @return int
+     */
+    public function move(string $direction, int $distance): int
+    {
+        $distance = min($distance, self::MAX_MOVE_DISTANCE);
+
+        return $this->room->handleRobotMove($direction, $distance);
     }
 
 }
--- src/Entity/Room.php
+++ src/Entity/Room.php
@@ -4,20 +4,32 @@ namespace Area51\Entity;
 
 class Room
 {
-    /** @var int */
+    const DIRECTION_UP    = 'up';
+    const DIRECTION_DOWN  = 'down';
+    const DIRECTION_LEFT  = 'left';
+    const DIRECTION_RIGHT = 'right';
+
+    /**
+     * @var int
+     */
     private $width;
 
-    /** @var int */
+    /**
+     * @var int
+     */
     private $height;
 
-    /** @var int */
+    /**
+     * @var int
+     */
     private $robotX;
 
-    /** @var int */
+    /**
+     * @var int
+     */
     private $robotY;
 
     /**
-     * Room constructor.
      * @param $width
      * @param $height
      */
@@ -28,10 +40,103 @@ class Room
         $this->initRobot();
     }
 
+    /**
+     * init Robot position in Room
+     */
     private function initRobot()
     {
         $this->robotX = rand(0, $this->width - 1);
         $this->robotY = rand(0, $this->height - 1);
     }
 
+    /**
+     * @return array
+     */
+    public static function getValidDirections()
+    {
+        return [
+            self::DIRECTION_UP,
+            self::DIRECTION_DOWN,
+            self::DIRECTION_LEFT,
+            self::DIRECTION_RIGHT,
+        ];
+    }
+
+    /**
+     * handle Robot movement
+     *
+     * @param $direction
+     * @param $distance
+     * @return int
+     */
+    public function handleRobotMove($direction, $distance): int
+    {
+        if (self::DIRECTION_LEFT == $direction) {
+            return $this->robotMovesLeft($distance);
+        }
+
+        if (self::DIRECTION_UP === $direction) {
+            return $this->robotMovesUp($distance);
+        }
+
+        if (self::DIRECTION_RIGHT == $direction) {
+            return $this->robotMovesRight($distance);
+        }
+
+        if (self::DIRECTION_DOWN === $direction) {
+            return $this->robotMovesDown($distance);
+        }
+
+        return 0;
+    }
+
+    /**
+     * @param int $distance
+     * @return int
+     */
+    private function robotMovesLeft($distance): int
+    {
+        $distance = min($this->robotX, $distance);
+        $this->robotX -= $distance;
+
+        return $distance;
+    }
+
+    /**
+     * @param int $distance
+     * @return int
+     */
+    private function robotMovesUp($distance): int
+    {
+        $distance = min($this->robotY, $distance);
+        $this->robotY -= $distance;
+
+        return $distance;
+    }
+
+    /**
+     * @param int $distance
+     * @return int
+     */
+    private function robotMovesRight($distance): int
+    {
+        $wallDistance = ($this->width - 1) - $this->robotX;
+        $distance = min($wallDistance, $distance);
+        $this->robotX += $distance;
+
+        return $distance;
+    }
+
+    /**
+     * @param int $distance
+     * @return int
+     */
+    private function robotMovesDown($distance): int
+    {
+        $wallDistance = ($this->height - 1) - $this->robotY;
+        $distance = min($wallDistance, $distance);
+        $this->robotY += $distance;
+
+        return $distance;
+    }
 }
